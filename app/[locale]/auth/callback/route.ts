f5c74b7d6cc6c516f19246d423ef5c5349dbc40b
DianannaK <dianakertsman@gmail.com>
2025-12-09 22:46:54 +0200

    fix supabase callback arguments

--- app/[locale]/auth/callback/route.ts
+++ app/[locale]/auth/callback/route.ts
@@ -1,14 +1,24 @@
-import { createServerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import { createServerClient } from "@supabase/auth-helpers-nextjs";
 
 export async function GET(request: Request) {
   const cookieStore = cookies();
 
-  const supabase = createServerClient({
-    cookies: () => cookieStore,
-  });
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
+  );
 
-  // After successful login, redirect to homepage
-  return NextResponse.redirect(new URL('/', request.url));
+  // Make Supabase refresh session
+  await supabase.auth.getSession();
+
+  return NextResponse.redirect(new URL("/", request.url));
 }

60dad0f63e7ff0f44f0f6ee81f7eb8ac65dbe448
DianannaK <dianakertsman@gmail.com>
2025-12-10 10:25:15 +0200

    Remove readTime field from blog post generator

--- lib/data/blog.ts
+++ lib/data/blog.ts
@@ -163,7 +163,6 @@ for (let i = 0; i < 50; i++) {
     ALL_POSTS.push({ 
         id: idCounter.toString(),
         content: content,
-        readTime: 3 + Math.floor(Math.random() * 5),
         imageUrl: image,
         category: category as Category
     });
